01f698101eaba4775036f9df618bce0b870645e6
Oleh <olegx4@ukr.net>
2020-06-10 04:09:53 +0300

    Added simple table of topics. Configured proxy for local connection.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    state = {
+        isLoading: true,
+        topics: []
+    };
+
+    componentDidMount = async () => {
+        const response = await fetch('/api/topics');
+        const body = await response.json();
+        this.setState({topics: body, isLoading: false})
+    };
+
+    renderTableTopics() {
+        return this.state.topics.map(topic => {
+            const {id, name, description} = topic;
+            return (
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{name}</td>
+                    <td>{description}</td>
+                </tr>
+            )
+        })
+    }
+
+    render() {
+        const {isLoading} = this.state;
+
+        if (isLoading) {
+            return <p>Loading data ...</p>
+        }
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1>Topics list:</h1>
+                    <table id='topics'>
+                        <tbody>
+                            {this.renderTableTopics()}
+                        </tbody>
+                    </table>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
